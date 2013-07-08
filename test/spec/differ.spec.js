d65381ff2e927fb8fa12649e99027fffbba72953
Jeff Cross <middlefloor@gmail.com>
2013-07-08 16:55:48 -0700

    feat(checkArrayForDupe): Moved array duplicate checking into differ service.

--- test/spec/differ.spec.js
+++ test/spec/differ.spec.js
@@ -333,7 +333,7 @@ describe('Differ', function () {
         var delta = {
           newVal: ['foo', 'bar']
         };
-        
+
         differ.compareArrays({}, delta, function () {
           expect(delta.data).toEqual(['foo', 'bar']);
         });
@@ -504,5 +504,46 @@ describe('Differ', function () {
   it('should have a dictionary of event constants', function () {
     expect(syncEvents.GET).toBeDefined();
     expect(syncEvents.NONE).toBeDefined();
+  });
+
+
+  describe('$checkArrayForDupe', function () {
+    it('should exist', function () {
+      expect(!!differ.checkArrayForDupe).toBe(true);
+    });
+
+    it('should have the correct signature', function () {
+      var args = captureFunctionArgs(differ.checkArrayForDupe.toString());
+      expect(args[0]).toEqual('binder');
+      expect(args[1]).toEqual('delta');
+      expect(args[2]).toEqual('next');
+      expect(args[3]).toBeUndefined();
+    });
+
+    it('should add a duplicate property to delta if newValue is equal to binder.data', function () {
+      var binder = {data: ['foo', 'bar']}
+        , delta = {data: 'bar'}
+        , callback;
+
+      differ.checkArrayForDupe(binder, delta, function () {
+        expect(delta.duplicate).toBe(true);
+        callback = true;
+      });
+
+      expect(callback).toBe(true);
+    });
+
+    it('should not add a duplicate property to a delta if newVal is NOT equal to binder.data', function () {
+      var binder = {data: ['foo']}
+        , delta = {newVal: ['bar']}
+        , callback;
+
+      differ.checkArrayForDupe(binder, delta, function () {
+        expect(!delta.duplicate).toBe(true);
+        callback = true;
+      });
+
+      expect(callback).toBe(true);
+    })
   });
 });

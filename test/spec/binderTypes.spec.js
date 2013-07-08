a5a2c57466b6034fc6b5f96a2dde3c16f7927c71
Jeff Cross <middlefloor@gmail.com>
2013-07-08 09:13:46 -0700

    feat(binderTypes): Added service with constants for binder types.

---
+++ test/spec/binderTypes.spec.js
@@ -0,0 +1,15 @@
+describe('$binderTypes', function () {
+  var $binderTypes;
+  beforeEach(module('SyncResource'));
+  beforeEach(inject(function (_$binderTypes_) {
+    $binderTypes = _$binderTypes_;
+  }));
+
+  it('should exist', function () {
+    expect(!!$binderTypes).toBe(true);
+  });
+
+  it('should have collection type constant', function () {
+    expect($binderTypes.COLLECTION).toEqual('collection');
+  });
+});

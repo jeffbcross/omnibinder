003263d001251431e18fac2ed4cc5176baa0dfff
Jeff Cross <middlefloor@gmail.com>
2013-07-30 12:22:48 -0700

    feat(binderTypes): Added tests for new binder types.

--- test/spec/binderTypes.spec.js
+++ test/spec/binderTypes.spec.js
@@ -16,4 +16,24 @@ describe('$binderTypes', function () {
   it('should have object type constant', function () {
     expect($binderTypes.OBJECT).toEqual('object');
   });
+
+  it('should have boolean type constant', function () {
+    expect($binderTypes.BOOLEAN).toEqual('boolean');
+  });
+
+  it('should have string type constant', function () {
+    expect($binderTypes.STRING).toEqual('string');
+  });
+
+  it('should have number type constant', function () {
+    expect($binderTypes.NUMBER).toEqual('number');
+  });
+
+  it('should have binary type constant', function () {
+    expect($binderTypes.BINARY).toEqual('binary');
+  });
+
+  it('should have binary stream type constant', function () {
+    expect($binderTypes.BINARY_STREAM).toEqual('binaryStream');
+  });
 });

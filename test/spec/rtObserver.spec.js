70fce18652fb0e3ace6d3ff0dd196afe0562540a
Jeff Cross <middlefloor@gmail.com>
2013-08-22 12:29:59 -0700

    RT-35: Created observer service with basic support for watching arrays.

---
+++ test/spec/rtObserver.spec.js
@@ -0,0 +1,44 @@
+describe('rtObserver', function () {
+  var rtObserver, scope, binderTypes;
+
+  beforeEach(module('Binder'));
+  beforeEach(inject(function ($rootScope, _rtObserver_, _binderTypes_) {
+    scope = $rootScope;
+    binderTypes = _binderTypes_;
+    rtObserver = _rtObserver_;
+  }));
+
+
+  it('should exist', function () {
+    expect(!!rtObserver).toBe(true);
+  });
+
+
+  describe('observeCollection', function () {
+    it('should be a function', function () {
+      expect(typeof rtObserver.observeCollection).toBe('function');
+    });
+
+
+    it('should immediately respond to model changes', function () {
+      var caller = {callback: function () {}};
+      var spy = spyOn(caller, 'callback');
+
+      scope.myModel = ['foobar'];
+
+      rtObserver.observeCollection(binderTypes.COLLECTION, scope, 'myModel', caller.callback);
+
+      scope.myModel.push('baz');
+      scope.$digest();
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+
+  describe('unObserveCollection', function () {
+    it('should exist', function () {
+
+    });
+  });
+})
